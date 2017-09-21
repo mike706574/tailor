01cd0a4be87bd229302255f5e4a168b18ef2e7fb
EAS7510 <michaeleaster@northwesternmutual.com>
2017-09-20 20:21:03 -0500

    Snap.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mike706574/tailor "0.0.14"
+(defproject org.clojars.mike706574/tailor "0.0.15-SNAPSHOT"
   :description "Potentially useful string predicates and conformers."
   :url "https://github.com/mike706574/tailor"
   :license {:name "Eclipse Public License"
